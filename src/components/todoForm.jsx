2af6ca743e093eed0290e572c79e11368193fbc6
Mrsschalkwyk <96808682+Mrsschalkwyk@users.noreply.github.com>
2022-07-30 09:35:59 +1000

    30/07

---
+++ src/components/todoForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TodoForm() {
+    return (
+        <form>
+            <input type="Text" placeholder="Enter"></input>
+        </form>
+    )
+}
+
+export default TodoForm
